e9bf1e0a3c70485b06326cf23ffbfb5f8ff5ac70
Kenneth Li <kennethli.3470@gmail.com>
2017-01-06 15:19:43 -0500

    Added basic string input for overlord

--- overlord.c
+++ overlord.c
@@ -3,8 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 
+#define MAX_INPUT 1024
+
+char *prompt() {
+	printf("Enter command: ");
+
+	char *input = (char *) calloc(MAX_INPUT, sizeof(char));
+	fgets(input, MAX_INPUT, stdin);
+	input[strcspn(input, "\n")] = 0;
+	return input;
+}
+
 int main() {
+	int isRunning = 1;
 
-
+	while (isRunning) {
+		char *input = prompt();
+		printf("%s\n", input);
+		free(input);
+	}
+	
 	return 0;
 }
