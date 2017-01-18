b185be8b6bf9c367a9405dec06f7006e60dc1b5c
Kenneth Li <kennethli.3470@gmail.com>
2017-01-18 15:07:59 -0500

    Skeleton of overlord UI

--- overlord.c
+++ overlord.c
@@ -7,9 +7,14 @@
 
 #define MAX_INPUT 1024
 
-char *prompt() {
-	printf("Enter command: ");
+#define EXIT 0
+#define DEFAULT 1
+#define ORDER 2
+#define SPY 3
+#define MANAGE 4
 
+char *prompt(char *prefix) {
+  printf("%s", prefix);
 	char *input = (char *) calloc(MAX_INPUT, sizeof(char));
 	fgets(input, MAX_INPUT, stdin);
 	input[strcspn(input, "\n")] = 0;
@@ -20,29 +25,74 @@ void subServer(int connection) {
 
 }
 
+int startsWith(char *s1, char *s2) {
+  return strncmp(s1, s2, strlen(s2)) == 0;
+}
+
 int main() {
-  int sock = serverSocket(5001);
+  printf("Welcome, overlord.\n");
+  printf("Type help for available commands.\n");
 
-	int isRunning = 1;
+  int state = DEFAULT;
 
-	while (isRunning) {
-    int connection = serverConnect(sock);
+	while (state) {
+    switch (state) {
+    case DEFAULT:
+      char *input = prompt("[DEFAULT]> ");
 
-    int pid = fork();
+      if (startsWith(input, "help")) {
+      
+      } else if (startsWith(input, "order")) {
+        state = ORDER;
+      } else if (startsWith(input, "spy")) {
+        state = SPY;
+      } else if (startsWith(input, "manage")) {
+        state = MANAGE;
+      } else if (startsWith(input, "exit")) {
+        state = EXIT;
+      }
 
-    if (pid == 0) {
-      // child
-      close(sock);
-      subServer(connection);
-      exit(0);
-    } else {
-      // parent
-      close(connection);
+      break;
+
+    case ORDER:
+
+      break;
+
+    case SPY:
+
+      break;
+
+    case MANAGE:
+      char *input = prompt("[MANAGE]> ");
+
+      if (startsWith(input, "add")) {
+        int sock = serverSocket(5001);
+
+        int connection = serverConnect(sock);
+
+        int pid = fork();
+
+        if (pid == 0) {
+          // child
+          close(sock);
+          subServer(connection);
+          exit(0);
+        } else {
+          // parent
+          close(connection);
+        }
+      } else if (startsWith(input, "remove")) {
+
+      } else if (startsWith(input, "back")) {
+        state = DEFAULT;
+      }
+      
+      break;
+
+    default:
+
+      break;
     }
-    
-		char *input = prompt();
-		printf("%s\n", input);
-		free(input);
 	}
 	
 	return 0;
